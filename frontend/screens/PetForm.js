eb9b1b4a759a8a207846f9d86ba940a3198ae159
domus55 <dominik.palenik1@gmail.com>
2024-11-09 17:10:58 +0100

    Added possibility to edit petForm info

--- frontend/screens/PetForm.js
+++ frontend/screens/PetForm.js
@@ -1,5 +1,5 @@
 import { View, Text, KeyboardAvoidingView, ScrollView } from "react-native";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { formStyles } from "../styles.js/formStyles";
 import { green, white } from "../consts/colors";
 
@@ -9,9 +9,22 @@ import CustomButton from "../components/CustomButton";
 
 
 export default function CaregiverProfileForm() {
-   const [petName, setPetName] = useState('');
-   const [breed, setBreed] = useState('');
-   const [description, setDescription] = useState('');
+  // True if user uses this form for the first time and creating an account
+  // False if user already has an account and is editing it
+  const [editingProfile, setEditProfile] = useState(false)
+
+  const [formTitle, setFormTitle] = useState('')
+  const [petName, setPetName] = useState('');
+  const [breed, setBreed] = useState('');
+  const [description, setDescription] = useState('');
+
+  useEffect(() => {
+    if (editingProfile) {
+      setFormTitle("Edytuj dane swojego zwierzaka");
+    } else {
+      setFormTitle("Uzupełnij informacje żeby stworzyć profil zwierzaka");
+    }
+  }, [editingProfile]);
 
    return (
        <KeyboardAvoidingView 
@@ -21,7 +34,7 @@ export default function CaregiverProfileForm() {
            <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
                <View style={formStyles.container}>
                    <View style={[formStyles.middleSection, { justifyContent: "none" }]}>
-                       <Text style={formStyles.h1}>Uzupełnij informacje żeby stworzyć profil zwierzaka</Text>
+                       <Text style={formStyles.h1}>{formTitle}</Text>
                        <View style={formStyles.formContainer}>
                            <FormInput
                                value={petName}
--- frontend/screens/PetForm2.js
+++ frontend/screens/PetForm2.js
@@ -1,12 +1,19 @@
-import { useState } from 'react';
-import { Pressable, Image, View, StyleSheet, Text } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
+import { useState, useEffect } from 'react';
+import { Pressable, Image, View, Text } from 'react-native';
 import { formStyles } from "../styles.js/formStyles";
-import CustomButton from "../components/CustomButton";
 import { green, white } from "../consts/colors";
 
+import CustomButton from "../components/CustomButton";
+import * as ImagePicker from 'expo-image-picker';
+
+
 export default function PetForm2() {
+  // True if user uses this form for the first time and creating an account
+  // False if user already has an account and is editing it
+  const [editingProfile, setEditProfile] = useState(false)
+
   const img = require("../assets/default_dog_picture.png")
+  const [formTitle, setFormTitle] = useState('')
   const [hasPhoto, setHasPhoto] = useState(false);
   const [image, setImage] = useState("null");
 
@@ -18,33 +25,38 @@ export default function PetForm2() {
       quality: 1,
     });
 
-    //console.log(result);
-
     if (!result.canceled) {
       setImage(result.assets[0].uri);
       setHasPhoto(true)
     }
   };
+
+  useEffect(() => {
+    if (editingProfile) {
+      setFormTitle("Edytuj zdjęcie swojego zwierzaka");
+    } else {
+      setFormTitle("Dodaj zdjęcie swojego zwierzaka");
+    }
+  }, [editingProfile]);
+
+
   return (
   <View style={formStyles.container}>
     <View style={[formStyles.middleSection, {justifyContent: "none"}]}>
-      <Text style={formStyles.h1}>Dodaj zdjęcie swojego zwierzaka.</Text>
+      <Text style={formStyles.h1}>{formTitle}</Text>
+
       <View style={[formStyles.formContainer, {justifyContent: 'center', alignItems: 'center'}]}>
-
-      <Pressable onPress={pickImage}>
-        {hasPhoto ?
-        (image && <Image source={{ uri: image }} style={[formStyles.image]} />) :
-        (<Image source={img} style={[formStyles.image]}/>)}
-      </Pressable>
-
+        <Pressable onPress={pickImage}>
+          {hasPhoto ?
+          (image && <Image source={{ uri: image }} style={[formStyles.image]} />) :
+          (<Image source={img} style={[formStyles.image]}/>)}
+        </Pressable>
         <CustomButton 
           color={green} 
           textColor={white}
           action={""}
           title={"Kontynuuj"}>
-      </CustomButton>
-
-
+        </CustomButton>
       </View>
     </View>
   </View>
