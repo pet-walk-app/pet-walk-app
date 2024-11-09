368d426a20b1f9c47af8e83db61e01682437ba68
domus55 <dominik.palenik1@gmail.com>
2024-11-09 16:38:33 +0100

    Added KeyboardAvoidingView to PetForm and CaregiverProfileForm

--- frontend/screens/CaregiverProfileForm.js
+++ frontend/screens/CaregiverProfileForm.js
@@ -1,40 +1,49 @@
-import { View, Text } from "react-native";
+import { View, Text, KeyboardAvoidingView, ScrollView } from "react-native";
 import { formStyles } from "../styles.js/formStyles";
+import { useState } from "react";
+import { green, white } from "../consts/colors";
+
 import FormBigInput from "../components/FormBigInput";
 import FormInput from "../components/FormInput";
-import { useState } from "react";
 import CustomButton from "../components/CustomButton";
-import { green, white } from "../consts/colors";
+
 
 export default function CaregiverProfileForm() {
    const [city, setCity] = useState('')
    const [description, setDescription] = useState('')
 
     return (
-        <View style={formStyles.container}>
-            <View style={[formStyles.middleSection, {justifyContent: "none"}]}>
-               <Text style={formStyles.h1}>Uzupełnij swoje informacje {"\n"}żeby stworzyć profil opiekuna.</Text>
-               <View style={formStyles.formContainer}>
-                  <FormInput
-                     value={city}
-                     setValue={setCity}
-                     placeholder={'Miasto'}>
-                  </FormInput>
-                  <FormBigInput 
-                     value={description}
-                     setValue={setDescription}
-                     placeholder={'Napisz kilka słów o sobie'}
-                     height={330}>
-                  </FormBigInput>
-               </View>
+      <KeyboardAvoidingView 
+           style={{ flex: 1 }} 
+           behavior="position"
+       >
+         <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+            <View style={formStyles.container}>
+                  <View style={[formStyles.middleSection, {justifyContent: "none"}]}>
+                     <Text style={formStyles.h1}>Uzupełnij swoje informacje {"\n"}żeby stworzyć profil opiekuna.</Text>
+                     <View style={formStyles.formContainer}>
+                        <FormInput
+                           value={city}
+                           setValue={setCity}
+                           placeholder={'Miasto'}>
+                        </FormInput>
+                        <FormBigInput 
+                           value={description}
+                           setValue={setDescription}
+                           placeholder={'Napisz kilka słów o sobie'}
+                           height={330}>
+                        </FormBigInput>
+                     </View>
 
-               <CustomButton 
-                  color={green} 
-                  textColor={white}
-                  action={""}
-                  title={'Kontynuuj'}>
-               </CustomButton>
-            </View>
-         </View>
+                     <CustomButton 
+                        color={green} 
+                        textColor={white}
+                        action={""}
+                        title={'Kontynuuj'}>
+                     </CustomButton>
+                  </View>
+               </View>
+         </ScrollView>
+       </KeyboardAvoidingView>
     );
 }
--- frontend/screens/PetForm.js
+++ frontend/screens/PetForm.js
@@ -1,46 +1,54 @@
-import { View, Text } from "react-native";
-import { formStyles } from "../styles.js/formStyles";
-import FormBigInput from "../components/FormBigInput";
-import FormInput from "../components/FormInput";
+import { View, Text, KeyboardAvoidingView, ScrollView } from "react-native";
 import { useState } from "react";
-import CustomButton from "../components/CustomButton";
+import { formStyles } from "../styles.js/formStyles";
 import { green, white } from "../consts/colors";
 
+import FormBigInput from "../components/FormBigInput";
+import FormInput from "../components/FormInput";
+import CustomButton from "../components/CustomButton";
+
+
 export default function CaregiverProfileForm() {
-   const [petName, setPetName] = useState('')
-   const [breed, setBreed] = useState('')
-   const [description, setDescription] = useState('')
+   const [petName, setPetName] = useState('');
+   const [breed, setBreed] = useState('');
+   const [description, setDescription] = useState('');
 
-    return (
-        <View style={formStyles.container}>
-            <View style={[formStyles.middleSection, {justifyContent: "none"}]}>
-               <Text style={formStyles.h1}>Uzupełnij informacje żeby stworzyć profil zwierzaka</Text>
-               <View style={formStyles.formContainer}>
-                  <FormInput
-                     value={petName}
-                     setValue={setPetName}
-                     placeholder={'Imię psa'}>
-                  </FormInput>
-                  <FormInput
-                     value={breed}
-                     setValue={setBreed}
-                     placeholder={'Rasa'}>
-                  </FormInput>
-                  <FormBigInput 
-                     value={description}
-                     setValue={setDescription}
-                     placeholder={'Opis. Nie zapomnij o jego charakterze pupila i przyjmowanych lekach.'}
-                     height={270}>
-                  </FormBigInput>
+   return (
+       <KeyboardAvoidingView 
+           style={{ flex: 1 }} 
+           behavior="position"
+       >
+           <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+               <View style={formStyles.container}>
+                   <View style={[formStyles.middleSection, { justifyContent: "none" }]}>
+                       <Text style={formStyles.h1}>Uzupełnij informacje żeby stworzyć profil zwierzaka</Text>
+                       <View style={formStyles.formContainer}>
+                           <FormInput
+                               value={petName}
+                               setValue={setPetName}
+                               placeholder={'Imię psa'}
+                           />
+                           <FormInput
+                               value={breed}
+                               setValue={setBreed}
+                               placeholder={'Rasa'}
+                           />
+                           <FormBigInput
+                               value={description}
+                               setValue={setDescription}
+                               placeholder={'Opis. Nie zapomnij o charakterze pupila i przyjmowanych lekach.'}
+                               height={270}
+                           />
+                       </View>
+                       <CustomButton 
+                           color={green} 
+                           textColor={white}
+                           action={""}
+                           title={'Kontynuuj'}
+                       />
+                   </View>
                </View>
-
-               <CustomButton 
-                  color={green} 
-                  textColor={white}
-                  action={""}
-                  title={'Kontynuuj'}>
-               </CustomButton>
-            </View>
-         </View>
-    );
+           </ScrollView>
+       </KeyboardAvoidingView>
+   );
 }
